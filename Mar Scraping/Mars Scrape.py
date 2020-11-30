2fcd8f1ed2a8e602398ede69e885bf117aabfad2
jocelyn4112 <68340393+jocelyn4112@users.noreply.github.com>
2020-11-29 20:19:22 -0500

    Flask I think

--- Mar Scraping/Mars Scrape.py
+++ Mar Scraping/Mars Scrape.py
@@ -1,5 +1,44 @@
 # coding: utf-8
 
 #Imports & Dependencies
-from splinter import Browser
-from bs4 import BeautifulSoup
+
+
+
+
+
+#URL
+def MarsImage():
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')  
+    pic = soup.find("img",)["src"]
+    featured_image_url = "https://www.jpl.nasa.gov" + image
+    return featured_image_url
+
+# Iterate through all pages
+for x in range(50):
+    # HTML object
+    html = browser.html
+    # Parse HTML with Beautiful Soup
+    soup = BeautifulSoup(html, 'html.parser')
+    # Retrieve all elements that contain book information
+    articles = soup.find_all('article', class_='product_pod')
+
+    # Iterate through each book
+    for article in articles:
+        # Use Beautiful Soup's find() method to navigate and retrieve attributes
+        h3 = article.find('h3')
+        link = h3.find('a')
+        href = link['href']
+        title = link['title']
+        print('-----------')
+        print(title)
+        print('http://books.toscrape.com/' + href)
+
+    # Click the 'Next' button on each page
+    try:
+        browser.click_link_by_partial_text('next')
+          
+    except:
+        print("Scraping Complete")
---
+++ Mar Scraping/app.py
@@ -0,0 +1,5 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import Mars_scrape
+
+#create a
